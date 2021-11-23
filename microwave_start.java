9da5658a5e6a70284e569bf4e519c517060885f8
Liam <90276819+liam-fletcher1@users.noreply.github.com>
2021-11-22 21:35:12 -0500

    updated start of java microwave program

--- microwave_start.java
+++ microwave_start.java
@@ -1,5 +1,5 @@
 /*
- * The Microwave program
+ * The microwave program
  *
  * @author  Liam Fletcher
  * @version 1.0
@@ -8,62 +8,70 @@
 
 import java.util.Scanner;
 
-public class Main {
+/**
+ * The Microwave program
+ */
 
-    public static void main(String[] args) {
-        //Constants
-        final double additionTime = 0.5;
-        double timeOne = 0;
-        double numberOf = 0;
+final class Main {
+
+    // Constants
+    public static final float sub = 60;
+    public static final float pizza = 45;
+    public static final float soup = 95;
+    public static final float secondsMin = 60;
+
+
+
+    public static void main(final String[] args) {
+        // Variables
+        String choice;
+        double amount;
+        float numberOf;
+        float Minutes = 0;
+        float Seconds = 0;
+        float plays = 1;
+        final double numberOfTimes = 0.5;
+
+        // Scanner objects
+        final Scanner One = new Scanner(System.in);
+        final Scanner Two = new Scanner(System.in);
 
         try {
-            Scanner scan = new Scanner(System.in);
+            do {
+                // User Input
+                System.out.println("Are you heating a sub, pizza, or soup?: ");
+                choice = One.nextLine();
+                System.out.println("How many " + choice + "(s) are you heating?(max:3): ");
+                amount = Two.nextFloat();
 
-
-            System.out.println("Are you heating a sub," +
-                " pizza or soup?:");
-            String itemType = scan.nextLine();
-
-            if (itemType.equals("sub")) {
-                timeOne = 1;
-            } else if (itemType.equals("pizza")) {
-                timeOne = 0.45;
-            } else if (itemType.equals("soup")) {
-                timeOne = 1.45;
-            } else {
-                System.out.println("Invaild input.");
-                return;
-            }
-
-            System.out.println("Enter the amount of food you are heating(max 3):");
-
-            // user input
-            Double quantityTemp = scan.nextDouble();
-            numberOf = quantityTemp;
-
-            if ((numberOf > 3) || (numberOf < 1)) {
-                System.out.println("Invaild input.");
-                return;
-            }
-
-            // Calculate heating time.
-            double time = (timeOne + 0.5 * timeOne * (numberOf - 1));
-
-            // Output
-            if (numberOf == 1) {
-                System.out.println("The item needs:");
-                if (time == 1) {
-                    System.out.println(time + " minute to reheat.");
+                // Process
+                numberOf = (float)((amount + 1) * numberOfTimes);
+                if ("sub".equals(choice)) {
+                    Minutes = (float) Math.floor(sub *
+                        numberOf / secondsMin);
+                    Seconds = (sub * numberOf /
+                        secondsMin - Minutes) * secondsMin;
+                } else if ("pizza".equals(choice)) {
+                    Minutes = (float) Math.floor(pizza *
+                        numberOf / secondsMin);
+                    Seconds = (pizza * numberOf /
+                        secondsMin - Minutes) * secondsMin;
+                } else if ("soup".equals(choice)) {
+                    Minutes = (float) Math.floor(soup *
+                        numberOf / secondsMin);
+                    Seconds = (soup * numberOf /
+                        secondsMin - Minutes) * secondsMin;
                 } else {
-                    System.out.println(time + " minutes to reheat.");
+                    System.out.println("Invalid input.");
+                    break;
                 }
-            } else {
-                System.out.println("The items need:");
-                System.out.println(time + " minutes to reheat.");
-            }
+                System.out.println("The total heating time is " + Minutes +
+                    " minutes and " + Seconds + " seconds.");
+                plays = 2;
 
+            } while (plays == 1);
         } catch (Exception e) {
-            System.out.println("Invaild input");
+            System.out.println("Invalid input.");
         }
     }
 }
